b3c34223bd2e8ae2aa67aa1edd31b77a2904132f
aminerachyd <aminerachyd99@gmail.com>
2020-06-22 17:19:57 +0100

    Ajout de la gestion des commentaires

--- main.c
+++ main.c
@@ -81,7 +81,8 @@ typedef struct {
     char mes[40]
 } Erreurs;
 
-// Déclaration de la structure du symbole courant, contient le code du symbole ( TOKEN ) et le nom du symbole
+// Déclaration de la structure du symbole courant
+// contient le code du symbole ( TOKEN ) et le nom du symbole
 typedef struct {
     CODES_LEX CODE;
     char NOM[20];
@@ -133,39 +134,39 @@ void Lettre();
 
 // Tableau des erreurs
 Erreurs MES_ERR[100] = {
-        {ID_ERR, "Erreur de syntaxe ID"},
-        {NUM_ERR, "Erreur de syntaxe NUM"},
-        {PROGRAM_ERR, "Erreur de syntaxe PROGRAM"},
-        {CONST_ERR, "Erreur de syntaxe CONST"},
-        {VAR_ERR, "Erreur de syntaxe VAR"},
-        {BEGIN_ERR, "Erreur de syntaxe BEGIN"},
-        {END_ERR, "Erreur de syntaxe END"},
-        {IF_ERR, "Erreur de syntaxe IF"},
-        {THEN_ERR, "Erreur de syntaxe THEN"},
-        {WHILE_ERR, "Erreur de syntaxe WHILE"},
-        {DO_ERR, "Erreur de syntaxe DO"},
-        {READ_ERR, "Erreur de syntaxe READ"},
-        {WRITE_ERR, "Erreur de syntaxe WRITE"},
-        {PV_ERR, "Erreur de syntaxe ;"},
-        {PT_ERR, "Erreur de syntaxe ."},
-        {PLUS_ERR, "Erreur de syntaxe +"},
-        {MOINS_ERR, "Erreur de syntaxe -"},
-        {MULT_ERR, "Erreur de syntaxe *"},
-        {DIV_ERR, "Erreur de syntaxe /"},
-        {VIR_ERR, "Erreur de syntaxe ,"},
-        {AFF_ERR, "Erreur de syntaxe :="},
-        {EG_ERR, "Erreur de syntaxe ="},
-        {INF_ERR, "Erreur de syntaxe <"},
-        {INFEG_ERR, "Erreur de syntaxe <="},
-        {SUP_ERR, "Erreur de syntaxe >"},
-        {SUPEG_ERR, "Erreur de syntaxe >="},
-        {DIFF_ERR, "Erreur de syntaxe <>"},
-        {PO_ERR, "Erreur de syntaxe ("},
-        {PF_ERR, "Erreur de syntaxe )"},
+        {ID_ERR,              "Erreur de syntaxe ID"},
+        {NUM_ERR,             "Erreur de syntaxe NUM"},
+        {PROGRAM_ERR,         "Erreur de syntaxe PROGRAM"},
+        {CONST_ERR,           "Erreur de syntaxe CONST"},
+        {VAR_ERR,             "Erreur de syntaxe VAR"},
+        {BEGIN_ERR,           "Erreur de syntaxe BEGIN"},
+        {END_ERR,             "Erreur de syntaxe END"},
+        {IF_ERR,              "Erreur de syntaxe IF"},
+        {THEN_ERR,            "Erreur de syntaxe THEN"},
+        {WHILE_ERR,           "Erreur de syntaxe WHILE"},
+        {DO_ERR,              "Erreur de syntaxe DO"},
+        {READ_ERR,            "Erreur de syntaxe READ"},
+        {WRITE_ERR,           "Erreur de syntaxe WRITE"},
+        {PV_ERR,              "Erreur de syntaxe ;"},
+        {PT_ERR,              "Erreur de syntaxe ."},
+        {PLUS_ERR,            "Erreur de syntaxe +"},
+        {MOINS_ERR,           "Erreur de syntaxe -"},
+        {MULT_ERR,            "Erreur de syntaxe *"},
+        {DIV_ERR,             "Erreur de syntaxe /"},
+        {VIR_ERR,             "Erreur de syntaxe ,"},
+        {AFF_ERR,             "Erreur de syntaxe :="},
+        {EG_ERR,              "Erreur de syntaxe ="},
+        {INF_ERR,             "Erreur de syntaxe <"},
+        {INFEG_ERR,           "Erreur de syntaxe <="},
+        {SUP_ERR,             "Erreur de syntaxe >"},
+        {SUPEG_ERR,           "Erreur de syntaxe >="},
+        {DIFF_ERR,            "Erreur de syntaxe <>"},
+        {PO_ERR,              "Erreur de syntaxe ("},
+        {PF_ERR,              "Erreur de syntaxe )"},
         {CONST_VAR_BEGIN_ERR, "Erreur dans le token CONST dans BEGIN"},
-        {INST_BEGIN_ERR, "Erreur dans le token BEGIN"},
-        {ERR_CAR_INC, "caractère	inconnu"},
-        {ERR_FICH_VIDE, "fichier	vide", "« IDF	très	long"}};
+        {INST_BEGIN_ERR,      "Erreur dans le token BEGIN"},
+        {ERR_CAR_INC,         "caractère inconnu"},
+        {ERR_FICH_VIDE,       "fichier vide"}};
 
 // Tableau des enumérations
 // Ce tableau sert uniquement à donner un équivalent en string des enumérations en haut
@@ -204,12 +205,19 @@ const char *enumName[] = {
         "ERREUR_TOKEN"
 };
 
+// Variable pour la lecture du fichier
+FILE *file;
+
 // Caractère courant, lit le fichier
 char Car_Cour;
-TSym_Cour Sym_Cour;
-int Car_Cour_Sym;
-FILE *fp;
 
+// Symbole courant, contient son nom ainsi que son token
+TSym_Cour Sym_Cour;
+
+int Car_Cour_Sym;
+
+// Fonction pour afficher une erreur
+// à partir du tableau d'enumerations des erreurs
 void Erreur(ERREURS ERR) {
     int ind_err = ERR;
     printf("Erreur numéro %d \t	: %s \n", ind_err, MES_ERR[ind_err].mes);
@@ -239,8 +247,8 @@ void Lire_nombre() {
     for (int i = 0; i < 20; i++) {
         Sym_Cour.NOM[i] = Car_Cour;
         // Passage au prochain caractère
-        Car_Cour = fgetc(fp);
-        if (Car_Cour < 33 || !Car_is_number()) { // TODO traiter le cas de la virgule
+        Car_Cour = fgetc(file);
+        if (Car_Cour < 33 || !Car_is_number()) {
             // Si le caractère n'est pas un nombre, on s'arrete
             break;
         }
@@ -256,7 +264,7 @@ void Lire_mot() {
     for (int i = 0; i < 20; i++) {
         Sym_Cour.NOM[i] = Car_Cour;
         // Passage au prochain caractère
-        Car_Cour = fgetc(fp);
+        Car_Cour = fgetc(file);
         if (Car_Cour < 33 || Car_is_special()) {
             // Si le caractère est un séparateur, on s'arrete
             break;
@@ -269,37 +277,30 @@ void Lire_mot() {
     if (!strcmp(Sym_Cour.NOM, "const")) {
         Sym_Cour.CODE = CONST_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "var")) {
         Sym_Cour.CODE = VAR_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "begin")) {
         Sym_Cour.CODE = BEGIN_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "end")) {
         Sym_Cour.CODE = END_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "if")) {
         Sym_Cour.CODE = IF_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "then")) {
         Sym_Cour.CODE = THEN_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "while")) {
         Sym_Cour.CODE = WHILE_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "do")) {
         Sym_Cour.CODE = DO_TOKEN;
@@ -309,18 +310,12 @@ void Lire_mot() {
     if (!strcmp(Sym_Cour.NOM, "read")) {
         Sym_Cour.CODE = READ_TOKEN;
         return;
-
     }
     if (!strcmp(Sym_Cour.NOM, "write")) {
         Sym_Cour.CODE = WRITE_TOKEN;
         return;
-
     }
     Sym_Cour.CODE = ID_TOKEN;
-}
-
-void Lire_chaine() { // TODO not used
-
 }
 
 void Lire_special() {
@@ -330,8 +325,7 @@ void Lire_special() {
     for (int i = 0; i < 20; i++) {
         Sym_Cour.NOM[i] = Car_Cour;
         // Passage au prochain caractère
-        Car_Cour = fgetc(fp);
-
+        Car_Cour = fgetc(file);
         if (!strcmp(Sym_Cour.NOM, "(")) {
             Sym_Cour.CODE = PO_TOKEN;
             return;
@@ -340,13 +334,11 @@ void Lire_special() {
             Sym_Cour.CODE = PF_TOKEN;
             return;
         }
-
         if (Car_Cour < 33 || Car_is_letter() || Car_is_number()) {
             // Si le caractère n'est pas un spécial, on s'arrete
             break;
         }
     }
-
     if (!strcmp(Sym_Cour.NOM, ";")) {
         Sym_Cour.CODE = PV_TOKEN;
         return;
@@ -403,21 +395,35 @@ void Lire_special() {
         Sym_Cour.CODE = DIFF_TOKEN;
         return;
     }
+    if (!strcmp(Sym_Cour.NOM, "{*")) { // Traitement des commentaires
+        while (1) {
+            Car_Cour = fgetc(file);
+            if (Car_Cour == 42) {
+                memset(Sym_Cour.NOM, 0, 20);
+                Sym_Cour.NOM[0] = Car_Cour;
+                Car_Cour = fgetc(file);
+                if (Car_Cour == 125) {
+                    Sym_Cour.NOM[1] = Car_Cour;
+                    Car_Cour = fgetc(file);
+                    return;
+                } else {
+                    Sym_Cour.CODE = ERREUR_TOKEN;
+                    return;
+                }
+            }
+        }
+    }
     Sym_Cour.CODE = ERREUR_TOKEN;
     return;
 }
 
-void Lire_errone() { // TODO not used
-
-}
-
 void Lire_Caractere() {
     // Test si le fichier n'existe pas
-    if (fp == NULL) {
+    if (file == NULL) {
         printf("File not found");
         return;
     }
-    Car_Cour = fgetc(fp);
+    Car_Cour = fgetc(file);
 }
 
 void Sym_Suiv() {
@@ -444,7 +450,7 @@ void Sym_Suiv() {
                 Sym_Cour.CODE = EOF_TOKEN;
                 return;
             }
-            Car_Cour = fgetc(fp);
+            Car_Cour = fgetc(file);
             memset(Sym_Cour.NOM, 0, 20);
             Sym_Cour.CODE = NULL_TOKEN;
         }
@@ -460,11 +466,14 @@ void AfficherToken(TSym_Cour sym_cour) {
 
 void Ouvrir_Fichier(char PATH[50]) {
     // Le fichier à lire, on met le code pascal dedans
-    fp = fopen(PATH, "r");
+    file = fopen(PATH, "r");
 }
 
 // Fonction pour tester un symbole
 void Test_Symbole(CODES_LEX CODE, ERREURS CODE_ERR) {
+    while (Sym_Cour.CODE == NULL_TOKEN) {
+        Sym_Suiv();
+    }
     if (Sym_Cour.CODE == CODE) {
         Sym_Suiv();
     } else
@@ -489,6 +498,9 @@ void BLOCK() {
 }
 
 void CONSTS() {
+    while (Sym_Cour.CODE == NULL_TOKEN) {
+        Sym_Suiv();
+    }
     switch (Sym_Cour.CODE) {
         case CONST_TOKEN:
             Sym_Suiv();
@@ -533,7 +545,10 @@ void INSTS() {
     Test_Symbole(END_TOKEN, END_ERR);
 }
 
-void INST() {   // TODO A tester
+void INST() {
+    while (Sym_Cour.CODE == NULL_TOKEN) {
+        Sym_Suiv();
+    }
     switch (Sym_Cour.CODE) {
         case BEGIN_TOKEN:
             INSTS();
@@ -607,7 +622,7 @@ void COND() {
     EXPR();
 }
 
-void RELOP() {  // TODO A tester
+void RELOP() {
     switch (Sym_Cour.CODE) {
         case EG_TOKEN:
             Test_Symbole(EG_TOKEN, EG_ERR);
@@ -632,7 +647,7 @@ void RELOP() {  // TODO A tester
     }
 }
 
-void EXPR() {   // TODO A tester
+void EXPR() {
     TERM();
     while (Sym_Cour.CODE == PLUS_TOKEN || Sym_Cour.CODE == MOINS_TOKEN) {
         ADDOP();
@@ -640,7 +655,7 @@ void EXPR() {   // TODO A tester
     }
 }
 
-void ADDOP() {  // TODO A tester
+void ADDOP() {
     switch (Sym_Cour.CODE) {
         case PLUS_TOKEN:
             Test_Symbole(PLUS_TOKEN, PLUS_ERR);
@@ -653,7 +668,7 @@ void ADDOP() {  // TODO A tester
     }
 }
 
-void TERM() { // TODO A tester
+void TERM() {
     FACT();
     while (Sym_Cour.CODE == MULT_TOKEN || Sym_Cour.CODE == DIV_TOKEN) {
         MULOP();
@@ -661,7 +676,7 @@ void TERM() { // TODO A tester
     }
 }
 
-void MULOP() {  // TODO A tester
+void MULOP() {
     switch (Sym_Cour.CODE) {
         case MULT_TOKEN:
             Test_Symbole(MULT_TOKEN, MULT_ERR);
@@ -674,7 +689,7 @@ void MULOP() {  // TODO A tester
     }
 }
 
-void FACT() {   // TODO A tester
+void FACT() {
     if (Sym_Cour.CODE == ID_TOKEN) {
         ID();
         return;
@@ -693,9 +708,11 @@ void FACT() {   // TODO A tester
 
 void ID() {
     Lettre();
+    // Le premier caractère du symbole étant une lettre
+    // On teste si le caractère suivant est soit une lettre soit un chiffre
     while (((Sym_Cour.NOM[Car_Cour_Sym] > 64 && Sym_Cour.NOM[Car_Cour_Sym] < 91) ||
             (Sym_Cour.NOM[Car_Cour_Sym] > 96 && Sym_Cour.NOM[Car_Cour_Sym] < 123)) ||
-           (Sym_Cour.NOM[Car_Cour_Sym] > 48 && Sym_Cour.NOM[Car_Cour_Sym] < 57)) { // TODO A tester
+           (Sym_Cour.NOM[Car_Cour_Sym] > 48 && Sym_Cour.NOM[Car_Cour_Sym] < 57)) {
         if (((Sym_Cour.NOM[Car_Cour_Sym] > 64 && Sym_Cour.NOM[Car_Cour_Sym] < 91) ||
              (Sym_Cour.NOM[Car_Cour_Sym] > 96 && Sym_Cour.NOM[Car_Cour_Sym] < 123))) {
             Lettre();
@@ -709,20 +726,22 @@ void ID() {
 
 void NUM() {
     Chiffre();
-    while ((Sym_Cour.NOM[Car_Cour_Sym] > 48 && Sym_Cour.NOM[Car_Cour_Sym] < 57)) { // TODO A tester
+    while ((Sym_Cour.NOM[Car_Cour_Sym] > 48 && Sym_Cour.NOM[Car_Cour_Sym] < 57)) {
         Chiffre();
     }
     Sym_Suiv();
 }
 
-void Chiffre() { // FIXME A tester
+void Chiffre() {
+    // Test si le premier caractère du symbole est un chiffre
     if (Sym_Cour.NOM[Car_Cour_Sym] > 48 && Sym_Cour.NOM[Car_Cour_Sym] < 57) {
         Car_Cour_Sym++;
         return;
     }
 }
 
-void Lettre() { // FIXME A tester
+void Lettre() {
+    // Test si le premier caractère du symbole est une lettre
     if ((Sym_Cour.NOM[Car_Cour_Sym] > 64 && Sym_Cour.NOM[Car_Cour_Sym] < 91) ||
         (Sym_Cour.NOM[Car_Cour_Sym] > 96 && Sym_Cour.NOM[Car_Cour_Sym] < 123)) {
         Car_Cour_Sym++;
@@ -734,9 +753,18 @@ int main() {
     Ouvrir_Fichier("pascal.p");
 
     //------------------------------------------
+    //       Test de l'analyseur lexical
+    //------------------------------------------
+    /*
+    Lire_Caractere();
+    while (Car_Cour != EOF) {
+        Sym_Suiv();
+        AfficherToken(Sym_Cour);
+    }*/
+
+    //------------------------------------------
     //       Test de l'analyseur syntaxique
     //------------------------------------------
-
     Lire_Caractere();
     Sym_Suiv();
     PROGRAM();
@@ -744,21 +772,7 @@ int main() {
         printf("BRAVO: Le programme est correct!");
     else
         printf("PAS	BRAVO: La fin de programme erronée!");
-
-
-    //------------------------------------------
-    //       Test de l'analyseur lexical
-    //------------------------------------------
-
-    /*
-    Lire_Caractere();
-    while (Car_Cour != EOF) {
-        Sym_Suiv();
-        AfficherToken(Sym_Cour);
-    }
-    */
-
-
-
     return 0;
 }
+
+
